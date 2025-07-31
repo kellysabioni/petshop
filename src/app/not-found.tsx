fae9213ca7e49725551c5e4094470878fc8d342a
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-31 11:32:33 -0300

    Atualiza página 404 com imagem, estilo e link para página inicial

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 //src/app/not-found.tsx
 import { Metadata } from "next";
 import styles from "./not-found.module.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Página não encontrada | PetShop",
@@ -13,6 +14,17 @@ export default function NotFound() {
   return (
     <section className={styles.conteudo}>
       <h2>Ops! Página não encontrada...</h2>
+
+      <div className={styles.imagem404}>
+        <img
+          src="/images/404.svg"
+          alt="Cachorrinho com cara triste e nariz vermelho, com símbolo de interrogação"
+        />
+      </div>
+
+      <p>
+        <Link href="/">Voltar para Página Inicial </Link>
+      </p>
     </section>
   );
 }
