a004672975700e0b9510548efb27c96c724fec83
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-31 11:00:52 -0300

    Aplica estilos em pagina sobre

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,5 +1,6 @@
 //src/app/sobre/page.tsx
 import { Metadata } from "next";
+import styles from "./sobre.module.css";
 
 export const metadata: Metadata = {
   title: "Sobre | PetShop",
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Sobre() {
   return (
-    <section>
+    <section className={styles.conteudo}>
       <h2>Sobre</h2>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus
