98d76302f2896e82e6e1a171a5d494924c188b29
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-31 11:00:06 -0300

    Aplica estilos em contatos

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,5 +1,6 @@
 //src/app/contato/page.tsx
 import { Metadata } from "next";
+import styles from "./contato.module.css";
 
 export const metadata: Metadata = {
   title: "Contato | PetShop",
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Contato() {
   return (
-    <section>
+    <section className={styles.conteudo}>
       <h2>Contato</h2>
       <p></p>
     </section>
