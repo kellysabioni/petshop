f098391ace6ec2fdd15da3446972ea2c56a47ddd
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-31 11:19:57 -0300

    Atualiza conteudo da página SOBRE

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -11,10 +11,22 @@ export default function Sobre() {
   return (
     <section className={styles.conteudo}>
       <h2>Sobre</h2>
+      <h3>Missão</h3>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus
-        harum cumque eligendi maxime dolorem, aspernatur enim ratione nisi,
-        fugiat minima officia accusantium eaque qui.
+        Nossa missão é proporcionar o melhor cuidado e bem-estar para os animais
+        de estimação, oferecendo produtos e serviços de alta qualidade que
+        atendam às necessidades dos nossos clientes e seus pets.
+      </p>
+      <h3>Visão</h3>
+      <p>
+        Nossa visão é ser reconhecida como a principal referência em cuidados
+        para animais de estimação, promovendo a saúde, felicidade e longevidade
+        dos nossos amigos de quatro patas.
+      </p>
+      <h3>Valores</h3>
+      <p>
+        Nossos valores incluem amor pelos animais, compromisso com a qualidade,
+        respeito ao meio ambiente e dedicação ao atendimento ao cliente.
       </p>
     </section>
   );
