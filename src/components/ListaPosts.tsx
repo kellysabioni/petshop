f06882e77b022ae012d929818f5b78549ecec302
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-01 09:41:53 -0300

    Aplica estilo em ListaPosts

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -1,4 +1,11 @@
 // src/components/ListaPosts.tsx
+import styles from "./ListaPosts.module.css";
+
 export default function ListaPosts() {
-  return <div>ListaPosts</div>;
+  return <div className={styles.posts} >
+    <article>
+        <h3>Titulo do post</h3>
+        <p>Subtitulo do post</p>
+    </article>
+  </div>;
 }
