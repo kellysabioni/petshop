4afc93e7d16f54328189f0580622d79216da1fa0
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-01 10:02:18 -0300

    Aplica type posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // src/app/page.tsx
 import ListaPosts from "@/components/ListaPosts";
 import styles from "./page.module.css";
+import arrayPosts from "@/data/array-posts";
+
 export default function Home() {
   return (
     <section className={styles.conteudo}>
       <h2>Pet Notícias</h2>
       <p>Aqui voce encontra as últimas notícias sobre Pets.</p>
-      <ListaPosts />
+      <ListaPosts posts={arrayPosts} />
     </section>
   );
 }
