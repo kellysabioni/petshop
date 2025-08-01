61d63abacada64781e08787166f6fe0c42a85074
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-01 11:37:16 -0300

    Aplica map

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -4,14 +4,20 @@ import styles from "./ListaPosts.module.css";
 
 /* Confirgurando um tipo para este componente e, neste tipo, definimos a prop posts relacionando ela ao tipo Post criado anteriormente  */
 type ListaPostsProps = {
-    posts: Post[];
-}
+  posts: Post[];
+};
 
-export default function ListaPosts({posts}: ListaPostsProps) {
-  return <div className={styles.posts} >
-    <article>
-        <h3>Titulo do post</h3>
-        <p>Subtitulo do post</p>
-    </article>
-  </div>;
+export default function ListaPosts({ posts }: ListaPostsProps) {
+  return (
+    <div className={styles.posts}>
+      {posts.map((post) => {
+        return (
+          <article key={post.id}>
+            <h3>{post.titulo}</h3>
+            <p>{post.subtitulo}</p>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
