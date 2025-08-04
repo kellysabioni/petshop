12a2a9e94b9be58487775706dfd0bfe1501d6b3f
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-04 11:00:58 -0300

    Resolve desafio Selecionar unico post

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,33 @@
 //sr/app/posts/[id]/page.tsx
 import Container from "@/components/Container";
 import styles from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
 
 type DetalhePostProps = {
-    params: Promise<{id: string}>
-}
-export default function DetalhePost({params}:DetalhePostProps) {
-  return (
-    <article className={styles.conteudo} >
-      <h2>Titulo</h2>
-      <Container>
-        <h3>Categoria</h3>
-        <p>Descrição</p>
-      </Container>
-    </article>
-  );
+    params: Promise<{ id: string }>;
+};
+
+export default async function DetalhePost({ params }: DetalhePostProps) {
+    const { id } = await params;
+
+    // 2. Faz um novo fetch na API usando o ID recebido
+    const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+        next: { revalidate: 0 },
+    });
+
+    if (!resposta.ok) {
+        throw new Error("Erro ao buscar o post: " + resposta.statusText);
+    }
+
+    const post: Post = await resposta.json();
+
+    return (
+        <article className={styles.conteudo}>
+            <h2>{post.titulo}</h2>
+            <Container>
+                <h3>{post.categoria}</h3>
+                <p>{post.descricao}</p>
+            </Container>
+        </article>
+    );
 }
