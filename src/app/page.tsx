0d1d45a4c733bcf4902d05e85dfedd7a12035302
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-04 09:17:04 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,26 @@
 import ListaPosts from "@/components/ListaPosts";
 import styles from "./page.module.css";
 import arrayPosts from "@/data/array-posts";
+import { Post } from "@/types/Post";
 
 export default async function Home() {
-  const resposta = await fetch(`http://localhost:2112/posts`);
+  const resposta = await fetch(`http://localhost:2112/posts`, {
+    /* Revalidamos o cache do next a cada requisição para garantir que os dados estejam sempre atualizados */
+    next: { revalidate: 0 },
+  });
 
   if (!resposta.ok) {
     throw new Error("Erro ao buscar os posts: " + resposta.statusText);
   }
 
-  const posts = await resposta.json();
+  const posts: Post[] = await resposta.json();
   console.log(posts);
 
   return (
     <section className={styles.conteudo}>
       <h2>Pet Notícias</h2>
       <p>Aqui voce encontra as últimas notícias sobre Pets.</p>
+
       <ListaPosts posts={arrayPosts} />
     </section>
   );
