3817b6825b294afab7e3b225ca98cb6fb9f4a0c0
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-04 11:36:18 -0300

    Gera Metadados de forma dinamica

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -4,30 +4,51 @@ import styles from "./detalhe-post.module.css";
 import { Post } from "@/types/Post";
 
 type DetalhePostProps = {
-    params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 };
 
+
+export async function generateMetadata({ params }: DetalhePostProps) {
+  const { id } = await params;
+
+  // 2. Faz um novo fetch na API usando o ID recebido
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar o post: " + resposta.statusText);
+  }
+
+  const post: Post = await resposta.json();
+
+  return {
+    title: post.titulo + "| PetShop",
+    description: post.descricao,
+  };
+}
+
 export default async function DetalhePost({ params }: DetalhePostProps) {
-    const { id } = await params;
+  const { id } = await params;
 
-    // 2. Faz um novo fetch na API usando o ID recebido
-    const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
-        next: { revalidate: 0 },
-    });
+  // 2. Faz um novo fetch na API usando o ID recebido
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
 
-    if (!resposta.ok) {
-        throw new Error("Erro ao buscar o post: " + resposta.statusText);
-    }
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar o post: " + resposta.statusText);
+  }
 
-    const post: Post = await resposta.json();
+  const post: Post = await resposta.json();
 
-    return (
-        <article className={styles.conteudo}>
-            <h2>{post.titulo}</h2>
-            <Container>
-                <h3>{post.categoria}</h3>
-                <p>{post.descricao}</p>
-            </Container>
-        </article>
-    );
+  return (
+    <article className={styles.conteudo}>
+      <h2>{post.titulo}</h2>
+      <Container>
+        <h3>{post.categoria}</h3>
+        <p>{post.descricao}</p>
+      </Container>
+    </article>
+  );
 }
