cd3f4dabc36632ae09bf51feedbce83a7614c689
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-08 11:20:59 -0300

    Atualiza not-found

--- src/app/posts/[id]/not-found.tsx
+++ src/app/posts/[id]/not-found.tsx
@@ -1,7 +1,17 @@
 //src/app/posts/[id]/not-found.tsx
-
+import Container from "@/components/Container";
+import styles from "./not-found.module.css";
+import Link from "next/link";
 export default function PostNaoEncontrado() {
   return (
-    <div>PostNaoEncontrado</div>
-  )
+    <section className={styles.conteudo}>
+      <h2>Ops! Post não encontrado</h2>
+      <Container>
+        <p>O Post que voce está tentando encontrar não existe </p>
+        <p>
+          <Link href="/">Voltar para a pagina principal</Link>
+        </p>
+      </Container>
+    </section>
+  );
 }
