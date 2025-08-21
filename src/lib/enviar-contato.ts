b39ae3b834c9c5d51b56c3373b715615906fa842
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-21 09:50:13 -0300

    Cria e configura ação de servidor

---
+++ src/lib/enviar-contato.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { supabase } from "./supabase";
+
+export async function enviarContato(dadosDoFormulario: FormData) {
+  /* Extraindo e tratando os dados de cada campo do formulário
+  get para acessar
+  ? valida que o campo existe
+  tostring valida o tipo 
+  trim elimina espaços */
+  const nome = dadosDoFormulario.get("nome")?.toString().trim();
+  const email = dadosDoFormulario.get("email")?.toString().trim();
+  const mensagem = dadosDoFormulario.get("mensagem")?.toString().trim();
+
+  /* Validação básica no back-end */
+  if (!nome || !email || !mensagem) {
+    throw new Error("Todos os campos são obrigatórios");
+  }
+
+  /* Executando o insert através da lib supabase. Capturamos também o error para fazer um tratamento básico de erros do Supabase */
+  const { error } = await supabase
+    .from("contatos")
+    .insert({ nome, email, mensagem });
+
+  if (error) {
+    throw new Error("Não foi possível enviar sua mensagem. Tente mais tarde.");
+  }
+}
